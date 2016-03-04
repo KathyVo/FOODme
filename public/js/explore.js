03b2f095866f26131aebeb89ce6b76bd6e5a1808
vagrant <vagrant@precise32.(none)>
2016-03-04 04:13:22 +0000

    testing pull

--- public/js/explore.js
+++ public/js/explore.js
@@ -98,7 +98,7 @@ $(document).ready(function() {
     //push new dish into your bookmarks
     $('#book_button').click(function() {
       //send google analytics on bookmark like
-      ga("send", "event", "bookmark", "click");
+      //ga("send", "event", "bookmark", "click");
       //alert that dish was bookmarked
       alert(curr.dishName + " bookmarked!");
       var newBMK = {
@@ -119,45 +119,13 @@ $(document).ready(function() {
           'restaurantName' : curr.restaurantName,
           'imgURL' : curr.imgURL,
           'address' : curr.address,
-          'id': curr.id
+
         }
       );
     });
 
-
-
-  //push new dish into your bookmarks from item page
-  $('#book_btn').click(function() {
-    //send google analytics on bookmark like
-    ga("send", "event", "bookmark", "click");
-    //alert that dish was bookmarked
-    alert(curr.dishName + " bookmarked!");
-    var newBMK = {
-      'dishName' : curr.dishName,
-      'restaurantName' : curr.restaurantName,
-      'imgURL' : curr.imgURL,
-      'address' : 'SD, CA'
-    }
-    console.log("BMK testing...");
-    console.log('rugt place!');
-    console.log(newBMK);
-
-    //store bookmarks in bookmark
-    $.get(
-      "/addBookmark",
-      {
-        'dishName' : curr.dishName,
-        'restaurantName' : curr.restaurantName,
-        'imgURL' : curr.imgURL,
-        'address' : curr.address
-      }
-    );
-  })
-
-  });
-
-
-
 $('#submitBtn').click(function() {
   alert("Rating Sent!");
+});
+
 });
